3815889fcbf11dd9cb9605deb310f5604172c8af
Mansi Saxena <Mansi.Saxena@bhavnacorp.com>
2022-08-31 17:12:12 +0530

    commit

---
+++ day6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace day6
+{
+    public class genericclass<T>
+    {
+        public T msg;
+        public int a = 10;
+        public void genericmethod(T name, T location)
+        {
+            Console.WriteLine(msg);
+            Console.WriteLine(name);
+            Console.WriteLine(location);
+            Console.WriteLine(a);
+
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                genericclass<string> gc = new genericclass<string>();
+                gc.msg = "Welcome ";
+                gc.genericmethod("mansi", "aligarh");
+            }
+        }
+    }
+}
